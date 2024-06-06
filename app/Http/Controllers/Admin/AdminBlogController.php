0f3318aa6703d6974ba49b31523ae0401afc353f
shuto kikai <yysskikai04@icloud.com>
2024-06-06 16:11:40 +0900

    commit

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -10,14 +10,16 @@ use App\Http\Requests\Admin\UpdateBlogRequest;
 use Illuminate\Support\Facades\Storage; 
 use App\Models\Category;
 use App\Models\Cat;
+use Illuminate\Support\Facades\Auth; 
 
 class AdminBlogController extends Controller
 {
     //ブログ一覧画面
     public function index()
     {
+			$user = Auth::user();
 			$blogs = Blog::latest('updated_at')->simplePaginate(10);
-      return view('admin.blogs.index', ['blogs' => $blogs]);
+      return view('admin.blogs.index', ['blogs' => $blogs, 'user' => $user]);
     }
 
     //ブログ投稿画面
