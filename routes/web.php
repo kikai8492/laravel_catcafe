c62b14356f04c919f2be6acabd483ded20f3b112
shuto kikai <yysskikai04@icloud.com>
2024-06-07 20:54:15 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/contact/complete',[ContactController::class, 'complete']) -> name('
 Route::get('/admin/blogs',[AdminBlogController::class, 'index'])->name('admin.blogs.index')->middleware('auth');
 Route::get('/admin/blogs/create',[AdminBlogController::class, 'create'])->name('admin.blogs.create');
 Route::post('/admin/blogs', [AdminBlogController::class, 'store']) ->name('admin.blogs.store');
-Route::get('/admin/blogs/{blog}',[AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
+Route::get('/admin/blogs/{blog}',[AdminBlogController::class, 'edit'])->name('admin.blogs.edit')->middleware('auth');
 Route::put('/admin/blogs/{blog}',[AdminBlogController::class, 'update'])->name('admin.blogs.update');
 Route::delete('/admin/blogs/{blog}',[AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
 
@@ -27,6 +27,5 @@ Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users
 //認証
 Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
 Route::post('/admin/login', [AuthController::class, 'login']);
-
 //ログアウト
 Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
